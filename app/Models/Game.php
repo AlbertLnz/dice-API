96ab5fac73cf2360b693bee06d879371ba5c5706
AlbertLnz <albert2000_lanza@gmail.com>
2023-05-20 08:13:32 +0200

    Simple Logic Game on Model

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+
+    public $dice1;
+    public $dice2;
+    public $numberResult;
+    public $textResult;
+
+    public function __construct(){
+        $this->dice1 = $this->throwDice();
+        $this->dice2 = $this->throwDice();
+        $this->numberResult = $this->dice1 + $this->dice2;
+        $this->textResult = $this->stringResult();
+    }
+
+    public function throwDice():int{
+        return rand(1,6);
+    }
+
+    public function stringResult():string{
+        if($this->numberResult == 7){
+            return "Win";
+        }else{
+            return "Lose";
+        }
+    }
+
 }
