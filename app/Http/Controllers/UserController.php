450200bf344871a5b2027340590556ee4b81d0a4
AlbertLnz <albert2000_lanza@gmail.com>
2023-05-20 10:11:34 +0200

    UserController index function & his route [List of all players w/ average success rate]

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use App\Models\Game;
+use App\Http\Services\UserService;
+
+class UserController extends Controller
+{
+    //ADMIN ROUTES
+    public function index(){
+
+        $allPlayers = User::all(); //object
+        $allGames = Game::all(); //object
+        $playersList=array();
+        $userServiceMethods = new UserService;
+
+        for($i=0 ; $i<count($allPlayers) ; $i++){
+            
+            if(isset($allPlayers[$i]['id'])){
+                $allGamesFiltred = $allGames->where('user_id', $i+1);
+                $allPlayers[$i]['winRate'] = $userServiceMethods->percentageWinRate($allGamesFiltred, $i);
+                $allPlayers[$i]->save();
+                $userGames = User::with('games')->find($i+1);
+
+                array_push($playersList, $userGames);
+            }
+        }
+        
+        return $playersList;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Admin Routes
+Route::get('players', [UserController::class, 'index'])->name('api.players.index');
