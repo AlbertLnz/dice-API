1b6281d617e5810f476baf4a59292c178d3c5c12
AlbertLnz <albert2000_lanza@gmail.com>
2023-05-23 19:59:35 +0200

    2 Login tests (correct & incorrect)

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -7,9 +7,11 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Artisan;
 use App\Models\User;
+use Spatie\Permission\Models\Role;
 
 class UserTest extends TestCase
 {
+    //REGISTER (INCORRECT)
     public function test_bad_register(){
         $response = $this->post(route('api.register'),[
             'email' => 'xxxx',
@@ -18,6 +20,7 @@ class UserTest extends TestCase
         $response->assertStatus(302); 
     }
 
+    //REGISTER (CORRECT)
     public function test_ok_register_being_anonymous(){
 
         $response = $this->post(route('api.register'),[
@@ -28,4 +31,31 @@ class UserTest extends TestCase
         $this->assertEquals($response->json('user')['name'], 'anonymous');
         $response->assertJsonStructure(['token'])->assertOk();
     }
+
+    //LOGIN (INCORRECT)
+    public function test_bad_login(){
+        $user = User::factory()->create();
+
+        $response = $this->postJson(route('api.login'),[
+            'email' => $user->email,
+            'password' => "incorrect_password"
+        ]);
+        
+        $response->assertJsonStructure([])->assertStatus(401); //no token in Json
+
+    }
+
+    //LOGIN (CORRECT)
+    public function test_ok_login(){
+
+        $user = User::factory()->create();
+
+        $response = $this->postJson(route('api.login'),[
+            'email' => $user->email,
+            'password' => "password"
+        ]);
+        
+        $response->assertJsonStructure(['token'])->assertOk();
+    }
+
 }
